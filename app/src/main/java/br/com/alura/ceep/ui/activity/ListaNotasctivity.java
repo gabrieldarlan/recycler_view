91179f715df16455c8b648b63fc6e681b5c45096
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-05-19 15:10:54 -0300

    cap 3 completo

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasctivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasctivity.java
@@ -1,33 +1,44 @@
 package br.com.alura.ceep.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 import br.com.alura.ceep.R;
 import br.com.alura.ceep.dao.NotaDAO;
 import br.com.alura.ceep.model.Nota;
-import br.com.alura.ceep.ui.adapter.ListaNotasAdapter;
+import br.com.alura.ceep.ui.recyclerview.adapter.ListaNotasAdapter;
 
 public class ListaNotasctivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Ceep";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notasctivity);
+        setTitle(TITULO_APPBAR);
+        List<Nota> todasNotas = notasDeExemplo();
+        configuraRecyclerView(todasNotas);
+    }
 
-        ListView listaNotas = findViewById(R.id.listView);
+    private List<Nota> notasDeExemplo() {
         NotaDAO dao = new NotaDAO();
-        for (int i = 1; i <= 10000; i++) {
-            dao.insere(new Nota("Titulo " + i, "Descrição " + i));
-        }
+        dao.insere(new Nota("Primeira nota","Descrição pequena"));
+        dao.insere(new Nota("Segunda nota","Segunda descrição maior pq sim"));
+        return dao.todos();
+    }
 
-        List<Nota> todasNotas = dao.todos();
+    private void configuraRecyclerView(List<Nota> todasNotas) {
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
+        configuraAdapter(todasNotas, listaNotas);
+    }
+
+    private void configuraAdapter(List<Nota> todasNotas, RecyclerView listaNotas) {
         listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
-
     }
 }
