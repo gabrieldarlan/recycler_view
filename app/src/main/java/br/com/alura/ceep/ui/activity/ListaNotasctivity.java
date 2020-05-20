68316c00b3d1b033b98905c6c2fbdcfddda84a12
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-05-19 21:07:59 -0300

    adicionando notas na lista

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasctivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasctivity.java
@@ -1,9 +1,11 @@
 package br.com.alura.ceep.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -20,10 +22,28 @@ public class ListaNotasctivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_notasctivity);
+        setContentView(R.layout.activity_lista_notas);
         setTitle(TITULO_APPBAR);
         List<Nota> todasNotas = notasDeExemplo();
         configuraRecyclerView(todasNotas);
+
+        TextView botaoInsereNota = findViewById(R.id.lista_notas_insere_nota);
+        botaoInsereNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iniciaFormularioNota =
+                        new Intent(ListaNotasctivity.this, FormularioNotaActivity.class);
+                startActivity(iniciaFormularioNota);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        NotaDAO dao = new NotaDAO();
+        List<Nota> todos = dao.todos();
+        configuraRecyclerView(todos);
+        super.onResume();
     }
 
     private List<Nota> notasDeExemplo() {
